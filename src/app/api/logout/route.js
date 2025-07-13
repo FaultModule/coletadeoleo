3ea6b4894f1d493f3f3c824dddd7b77fb9d4475a
Fernando Muniz <fernando.hon@live.com>
2025-07-12 22:53:53 -0300

    1.1.6

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const response = NextResponse.redirect(new URL("/login", "https://coletadeoleo.vercel.app"));
+  const response = NextResponse.redirect("/login");
 
   // Apagar o cookie 'token'
   response.cookies.set("token", "", {
